8d9bf1d7d4a1ace844e6cfe426c68a151fcaec39
DadBytes <bpmckim@icloud.com>
2025-11-25 21:28:24 -0500

    add basic function to return whole book

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("greetings boots")
+def get_book_text(filepath: str) -> str:
+    with open(filepath) as f:
+        file_contents = f.read()
+    return file_contents
+
+def main():
+    book_path = "books/frankenstein.txt"
+    print(get_book_text(book_path))
+    
+main()
